44939502669a40a5e9651f9d2965a7e7ba68c063
Vitor Acer5 <vitor.venante@gmail.com>
2026-03-10 22:04:10 -0300

    elaboração da estrutura dos arquivos

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprendendo CRUD</title>
+</head>
+<body>
+    <?php
+        require_once 'conexao.php';
+        require_once 'funcoes.php';
+
+        
+    ?>
+</body>
+</html>
