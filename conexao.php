2380db0736969fd901e3e022265755da18de25d5
Vitor Acer5 <vitor.venante@gmail.com>
2026-03-11 22:05:20 -0300

    no message

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 
     // variavel que ira representar a conexao com o Db
-    $conexao = mysqli_connect('localhost', 'root', '' ,'crud_estudo');
+    $conexao = mysqli_connect('localhost', 'root', '' ,'crud_estudo'); // 'Onde está o banco?', 'Quem vai acessar?', 'Qual a senha?', 'Qual banco eu qeuro usar?'
 
     if(!$conexao){
         die ("#Erro de conexão" . mysqli_connect_error());
