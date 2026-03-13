5d69a686b177e365e303499191a1e7a4ec17cdde
Vitor Acer5 <vitor.venante@gmail.com>
2026-03-12 23:22:34 -0300

    elaborado create do CRUD

--- salvar.php
+++ salvar.php
@@ -1,5 +1,29 @@
 <?php
 
-    var_dump($_POST);
+    require_once 'conexao.php';
+
+    if(isset($_POST['nome']) && isset($_POST['email'])) {
+        
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+
+
+        // 2 etapas do CREATE
+        // 1ª etapa: Montar a Query
+        $sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
+
+        // 2º etapa: Executar a Query (conexao + query)
+        mysqli_query($conexao, $sql);
+
+        // validacao da query ( true ou false )
+        $resultadoQuery = mysqli_query($conexao, $sql);
+
+        if($resultadoQuery){
+            echo "Usuário cadastrado com sucesso!";
+        } else 
+        {
+            echo "erro ao cadastrar usuário";
+        }
+    }
 
 ?>
