1a0b2b31d1af541ae9a05c62bad2b8ca9b7d30fe
Vitor Acer5 <vitor.venante@gmail.com>
2026-03-16 20:25:18 -0300

    finalizacao delete -  cruD

--- delete.php
+++ delete.php
@@ -1,5 +1,20 @@
 <?php
 
+    require_once 'conexao.php';
 
+    // verificar se id existe na URL
+    if(isset($_GET['id'])){
+        $id = (int) $_GET['id'];
+
+        // montar query DELETE    
+        $sql_delete = "DELETE FROM usuarios WHERE id=$id";
+
+        // executar Query
+        $resultado = mysqli_query($conexao, $sql_delete);
+        
+        // voltar pra listar.php
+        header("Location: listar.php");
+        exit;
+    }
 
 ?>
